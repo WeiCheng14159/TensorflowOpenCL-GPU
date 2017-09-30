0717afe0f91a9f9d97ccbb521a791476153ffb64
cheng wei <michael1996623@gmail.com>
2017-09-30 07:25:47 -0700

    Enable c++11 for comiplation

--- tensorflow/loader/android_build.sh
+++ tensorflow/loader/android_build.sh
@@ -2,7 +2,9 @@
 
 bazel build :loader \
    --verbose_failures \
+   --sandbox_debug \
    --crosstool_top=//external:android/crosstool \
    --host_crosstool_top=@bazel_tools//tools/cpp:toolchain \
    --cpu=arm64-v8a \
+   --cxxopt="-std=c++11" \
    --jobs=4
