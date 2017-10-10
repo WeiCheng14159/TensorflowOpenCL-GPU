78655b8cc2611cdb796d68b2e823e58a84053f30
cheng wei <michael1996623@gmail.com>
2017-10-10 08:04:29 -0700

    Delete unneccessary compiler flags

--- tensorflow/loader/android_build.sh
+++ tensorflow/loader/android_build.sh
@@ -2,14 +2,11 @@
 
 bazel build :loader \
    --verbose_failures \
-   --verbose_explanations\
    --sandbox_debug \
    --crosstool_top=//external:android/crosstool \
    --host_crosstool_top=@bazel_tools//tools/cpp:toolchain \
    --cpu=arm64-v8a \
    --cxxopt="-std=c++11" \
-   --linkopt="-pthread" \
-   --copt="-DMDB_USE_ROBUST=0" \
    --jobs=4 
 
 adb push ../../bazel-bin/tensorflow/loader/loader /data/user/0/TF
