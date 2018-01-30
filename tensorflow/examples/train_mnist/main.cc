dc2dcafed317365aa2f8e15467890c8e6711e1f5
Wei Cheng <michael1996623@gmail.com>
2018-01-30 15:02:15 +0800

    feat-Load MNIST data into TF Tensor
    * Add TF Flags
    * Load batch of input data into the TF model
    * https://stackoverflow.com/questions/39148671/how-to-fill-a-tensor-in-c
    * https://medium.com/joytunes/deploying-a-tensorflow-model-to-android-69d04d1b0cba
    * https://medium.com/@daj/how-to-shrink-the-tensorflow-android-inference-library-cb698facf758
    * https://www.tensorflow.org/api_docs/cc/class/tensorflow/tensor

--- tensorflow/examples/train_mnist/main.cc
+++ tensorflow/examples/train_mnist/main.cc
@@ -30,7 +30,6 @@ By Cheng Wei on 2018/Jan/24
 
 // MNIST reader helper
 #include "mnist/mnist_reader.hpp"
-#define ROOT_DIR "/data/local/tmp"
 
 // These are all common classes it's handy to reference with no namespace.
 using tensorflow::Flag;
@@ -59,12 +58,44 @@ Status LoadGraph(const string& graph_file_name,
   return Status::OK();
 }
 
-int main() {
+int main(int argc, char* argv[]) {
 
-  string root_dir = ROOT_DIR;
-  std::cout << "Root directory: " << ROOT_DIR << std::endl;
+  string root_dir         = "/data/local/tmp/";
+  string graph_name       = "mnist_100_mlp.pb";
+  string mnist_dir        = root_dir + "MNIST_data/";
+  string input_layer      = "input";
+  string output_layer     = "output";
+  int32  input_width      = 28;
+  int32  input_height     = 28;
+  int32  batch_size       = 10;
+
+  std::vector<Flag> flag_list = {
+      Flag("root_dir",      &root_dir,
+        "interpret graph file names relative to this directory"),
+      Flag("graph_name",    &graph_name,    "graph to be executed"),
+      Flag("mnist_dir",     &mnist_dir,     "MNIST dataset directory"),
+      Flag("input_layer",   &input_layer,   "name of input layer"),
+      Flag("output_layer",  &output_layer,  "name of output layer"),
+      Flag("batch_size",    &batch_size,    "training batch size"),
+  };
+
+  string usage = tensorflow::Flags::Usage(argv[0], flag_list);
+  const bool parse_result = tensorflow::Flags::Parse(&argc, argv, flag_list);
+  if (!parse_result) {
+    LOG(ERROR) << usage;
+    return -1;
+  }
+
+  // We need to call this to set up global state for TensorFlow.
+  tensorflow::port::InitMain(argv[0], &argc, &argv);
+  if (argc > 1) {
+    LOG(ERROR) << "Unknown argument " << argv[1] << "\n" << usage;
+    return -1;
+  }
+
+  std::cout << "[Root directory] = " << root_dir << std::endl;
+
   // Prepare MNIST dataset
-    string mnist_dir = root_dir + "/MNIST_data/";
     std::cout << "[MNIST Dataset Directory] = " << mnist_dir << std::endl;
 
     // Load MNIST data
@@ -79,34 +110,58 @@ int main() {
       dataset.test_images.size() << std::endl;
     std::cout << "[MNIST Dataset] Num of Test     Labels = " <<
       dataset.test_labels.size() << std::endl;
-    std::cout << "[MNIST Dataset] Input Image Size       = " <<
-      std::sqrt( dataset.training_images[0].size() )         << "x" <<
-      std::sqrt( dataset.training_images[0].size() )         << std::endl;
 
-  // Load and initialize the model.
-  string graph_dir = "/mnist_100_mlp.pb";
+    input_width  = std::sqrt( dataset.training_images[0].size() );
+    input_height = std::sqrt( dataset.training_images[0].size() );
+
+    std::cout << "[MNIST Dataset] Input Image Size       = " <<
+      input_width << "x" << input_height << std::endl;
+
+  // Load and initialize TF model.
   std::unique_ptr<tensorflow::Session> session;
-  string graph_path = tensorflow::io::JoinPath(root_dir, graph_dir);
+  string graph_path = tensorflow::io::JoinPath(root_dir, graph_name);
   Status load_graph_status = LoadGraph(graph_path, &session);
   if (!load_graph_status.ok()) {
     LOG(ERROR) << load_graph_status;
     return -1;
   }
-  std::cout << "[TF Model File Loaded Dir] = " << graph_dir << std::endl;
+  std::cout << "[TF Model File Loaded From Directory] = " << graph_path << std::endl;
 
   // Load MNIST training data into TF Tensors
-  std::vector<Tensor> img_tensors;
+    // batch_tensor with dimenstion { batch_size, input_width * input_height }
+    // = { batch_size, 28*28 }
+    tensorflow::Tensor batch_tensor(tensorflow::DT_FLOAT,
+      tensorflow::TensorShape({batch_size, input_width * input_height}));
 
-  // for( auto i = 0 ; i < training_images.size() )
+    for( auto batch_idx = 0 ; batch_idx < dataset.training_images.size() / batch_size;
+      batch_idx ++ ){ // Batch loop
 
+      // batch vector with dimension { 1, batch_size x input_width x input_height }
+      std::vector<float> batch_vec;
 
-  // Actually run the image through the model.
-  // std::vector<Tensor> outputs;
-  // Status run_status = session->Run({{input_layer, resized_tensor}},
-  //                                  {output_layer}, {}, &outputs);
-  // if (!run_status.ok()) {
-  //   LOG(ERROR) << "Running model failed: " << run_status;
-  //   return -1;
-  // }
+      for ( auto batch_itr = 0 ; batch_itr < batch_size ; batch_itr ++ ){ // Within a Batch
 
-}
+        // vec with dim { 1, input_width * input_height }
+        std::vector<uint8_t> vec = dataset.training_images[ batch_idx * batch_size + batch_itr ];
+        for( auto pixel = 0 ; pixel < vec.size() ; pixel ++ ){
+          // Cast image data from uint_8 -> float
+          batch_vec.push_back( (float)( vec[pixel] ) );
+        }
+      } // Within a Batch
+
+      // Copy a single batch of data to TF Tensor
+      std::copy_n( batch_vec.begin(), batch_vec.size(), batch_tensor.flat<float>().data() );
+
+      // Check if Tensor is initialized
+      if( !batch_tensor.IsInitialized() ){
+        LOG(ERROR) << "Tensor not initialized" ;
+        return -1;
+      }
+
+      // Input Tensor info
+      std::cout << "Batch " << batch_idx << " loaded " << batch_tensor.DebugString()
+        << std::endl;
+
+  } // End of Batch loop
+
+} // End of main
