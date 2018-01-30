11ad36c999bff53fa193ef4c057e339e991829dc
Wei Cheng <michael1996623@gmail.com>
2018-01-30 18:51:32 +0800

    feat-Change python script generating TF model
    * Change variable names
    * Add batch size
    * Change variables initialization method

--- tensorflow/examples/train_mnist/gen_tf_mlp.py
+++ tensorflow/examples/train_mnist/gen_tf_mlp.py
@@ -37,39 +37,34 @@ def main(_):
   mnist = input_data.read_data_sets(FLAGS.data_dir)
 
   # Create the model
-  x = tf.placeholder(tf.float32, [None, 784])
-  W = tf.Variable(tf.zeros([784, 10]))
+  x = tf.placeholder(tf.float32, [None, 784], name="input")
+  y = tf.placeholder(tf.int64, [None], name="output")
+
+  W = tf.Variable(tf.truncated_normal([784, 10], stddev=0.1))
   b = tf.Variable(tf.zeros([10]))
-  y = tf.matmul(x, W) + b
+
+  y_out = tf.matmul(x, W) + b
 
   # Define loss and optimizer
-  y_ = tf.placeholder(tf.int64, [None])
+  cross_entropy = tf.losses.sparse_softmax_cross_entropy(labels=y, logits=y_out)
+  train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy, name="train")
 
-  # The raw formulation of cross-entropy,
-  #
-  #   tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(tf.nn.softmax(y)),
-  #                                 reduction_indices=[1]))
-  #
-  # can be numerically unstable.
-  #
-  # So here we use tf.losses.sparse_softmax_cross_entropy on the raw
-  # outputs of 'y', and then average across the batch.
-  cross_entropy = tf.losses.sparse_softmax_cross_entropy(labels=y_, logits=y)
-  train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
+  init_op = tf.initialize_variables(tf.all_variables(), name='init_all_vars_op')
 
-  sess = tf.InteractiveSession()
-  tf.global_variables_initializer().run()
+  sess = tf.Session()
+  sess.run(init_op)
 
+  batch_size=1000
   # Train
   for _ in range(100):
-    batch_xs, batch_ys = mnist.train.next_batch(100)
-    sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
+    batch_xs, batch_ys = mnist.train.next_batch(batch_size)
+    sess.run(train_step, feed_dict={x: batch_xs, y: batch_ys})
 
   # Test trained model
-  correct_prediction = tf.equal(tf.argmax(y, 1), y_)
+  correct_prediction = tf.equal(tf.argmax(y_out, 1), y)
   accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
   print(sess.run(accuracy, feed_dict={x: mnist.test.images,
-                                      y_: mnist.test.labels}))
+                                      y: mnist.test.labels}))
 
   tf.train.write_graph(sess.graph_def,
   						'./',
