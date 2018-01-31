27bea05402f876562a7511b8dbdc631a32547054
Wei Cheng <michael1996623@gmail.com>
2018-01-31 09:39:41 +0800

    feat-Refine mlp training script
    * Refined the MLP network
    * Add SUmmary Ops to monitor the training process
    * Update MLP TF model files

--- tensorflow/examples/train_mnist/gen_tf_mlp.py
+++ tensorflow/examples/train_mnist/gen_tf_mlp.py
@@ -31,16 +31,18 @@ import tensorflow as tf
 
 FLAGS = None
 
+img_size = 28
 
 def main(_):
   # Import data
   mnist = input_data.read_data_sets(FLAGS.data_dir)
+  logs_path = FLAGS.log_dir
 
   # Create the model
-  x = tf.placeholder(tf.float32, [None, 784], name="input")
+  x = tf.placeholder(tf.float32, [None, img_size * img_size ], name="input")
   y = tf.placeholder(tf.int64, [None], name="output")
 
-  W = tf.Variable(tf.truncated_normal([784, 10], stddev=0.1))
+  W = tf.Variable(tf.truncated_normal([ img_size * img_size, 10], stddev=0.1))
   b = tf.Variable(tf.zeros([10]))
 
   y_out = tf.matmul(x, W) + b
@@ -49,22 +51,43 @@ def main(_):
   cross_entropy = tf.losses.sparse_softmax_cross_entropy(labels=y, logits=y_out)
   train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy, name="train")
 
-  init_op = tf.initialize_variables(tf.all_variables(), name='init_all_vars_op')
+  correct_prediction = tf.equal(tf.argmax(y_out, 1), y)
+  accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32), name="test")
+
+  # Create a summary to monitor loss tensor
+  tf.summary.scalar("cross_entropy", cross_entropy)
+  # Create a summary to monitor accuracy tensor
+  tf.summary.scalar("accuracy", accuracy)
+  # Merge all summaries into a single op
+  merged_summary_op = tf.summary.merge_all()
+
+  init_op = tf.initialize_variables(tf.all_variables(), name='init')
 
   sess = tf.Session()
   sess.run(init_op)
 
-  batch_size=1000
+  summary_writer = tf.summary.FileWriter(logs_path,
+									   graph=sess.graph)
+  num_steps = 10000
+  setp_to_test = 10
+  batch_size = 100
+
   # Train
-  for _ in range(100):
+  for step in range(num_steps):
     batch_xs, batch_ys = mnist.train.next_batch(batch_size)
-    sess.run(train_step, feed_dict={x: batch_xs, y: batch_ys})
+
+    if step % setp_to_test == 0:
+        error, acc, summary = sess.run([ cross_entropy, accuracy, merged_summary_op ], \
+                                        feed_dict={ x: batch_xs, y: batch_ys })
+        print('step %d, training accuracy %f' % (step, acc))
+        summary_writer.add_summary(summary, step)
+    else:
+        ts, summary = sess.run([ train_step, merged_summary_op ], \
+                                feed_dict={ x: batch_xs, y: batch_ys })
 
   # Test trained model
-  correct_prediction = tf.equal(tf.argmax(y_out, 1), y)
-  accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-  print(sess.run(accuracy, feed_dict={x: mnist.test.images,
-                                      y: mnist.test.labels}))
+  print('test accuracy %f' % sess.run(accuracy, feed_dict={x: mnist.test.images,
+    y: mnist.test.labels}) )
 
   tf.train.write_graph(sess.graph_def,
   						'./',
@@ -74,5 +97,7 @@ if __name__ == '__main__':
   parser = argparse.ArgumentParser()
   parser.add_argument('--data_dir', type=str, default='/tmp/tensorflow/mnist/input_data',
                       help='Directory for storing input data')
+  parser.add_argument('--log_dir', type=str, default='/tmp/tensorflow_logs/mlpnet',
+                      help='Directory for training data')
   FLAGS, unparsed = parser.parse_known_args()
   tf.app.run(main=main, argv=[sys.argv[0]] + unparsed)
