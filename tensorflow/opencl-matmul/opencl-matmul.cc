0ca9c53e2aacb0f91bca91efa7b15abfc7be4be1
Wei Cheng <michael1996623@gmail.com>
2018-02-13 20:45:26 +0800

    feat-Compute MatMul on CPU & GPU
    * Compute MatMul of two matrices and compare the results

--- tensorflow/opencl-matmul/opencl-matmul.cc
+++ tensorflow/opencl-matmul/opencl-matmul.cc
@@ -22,26 +22,62 @@ int main(int argc, char* argv[]) {
     // Load graph into session
     TF_CHECK_OK(session->Create(graph_def));
 
-    // X matrix has shape 1 * mat_size
-    // X matrix has shape mat_size * 1
-    int mat_size = 10000;
-    Tensor Tx(DT_FLOAT, TensorShape({1 ,mat_size}));
-    Tensor Ty(DT_FLOAT, TensorShape({mat_size ,1}));
+    int bat = atoi( argv[1] );
+    const int feat_size = 784;
+    Tensor Tx (DT_FLOAT, TensorShape({bat       ,feat_size }));
+    Tensor Ty (DT_FLOAT, TensorShape({feat_size ,10       }));
 
-    // x_ & y_ are Eigen::Tensor
-    auto x_ = Tx.matrix<float>().setRandom();
-    auto y_ = Ty.matrix<float>().setRandom();
+    // Matrix initializaiotn
+    auto Tx_map = Tx.tensor<float, 2>();
+    for( int i = 0 ; i < bat ; i ++ ){
+      for( auto j = 0 ; j < feat_size ; j ++ ){
+        Tx_map(i, j) = (i==j) ? 1 : 0;
+      }
+    }
+    auto Ty_map = Ty.tensor<float, 2>();
+    for( int i = 0 ; i < feat_size ; i ++ ){
+      for( auto j = 0 ; j < 10 ; j ++ ){
+        Ty_map(i, j) = (i!=j) ? 1 : 0;
+      }
+    }
 
-    // Compute matrix multiplaction result
+    // Compute matrix multiplaction result using TF
     std::vector<Tensor> output;
     TF_CHECK_OK(session->Run({{"x", Tx}, {"y", Ty}}, {"matmul"}, {}, &outputs)); // Get cost
-    cout << "GPU result: " << outputs[0].scalar<float>()(0) << endl;
+    auto tf_res = outputs[0].matrix<float>();
+    cout << "TF result: \n" << tf_res << endl;
 
-    // Check on CPU
-    auto mat = x_ * y_ ;
-    cout << "CPU result: " << mat.sum() << endl ;
+    // Compute matrix multiplaction result using Eigen
+    auto Tx_mat = Eigen::Map<Eigen::Matrix<
+                              float,           /* scalar element type */
+                              Eigen::Dynamic,  /* num_rows is a run-time value */
+                              Eigen::Dynamic,  /* num_cols is a run-time value */
+                              Eigen::RowMajor  /* tensorflow::Tensor is always row-major */>>(
+                                Tx.flat<float>().data(),  /* ptr to data */
+                                Tx.dim_size(0),           /* num_rows */
+                                Tx.dim_size(1)            /* num_cols */);
+    auto Ty_mat = Eigen::Map<Eigen::Matrix<
+                              float,           /* scalar element type */
+                              Eigen::Dynamic,  /* num_rows is a run-time value */
+                              Eigen::Dynamic,  /* num_cols is a run-time value */
+                              Eigen::RowMajor  /* tensorflow::Tensor is always row-major */>>(
+                                  Ty.flat<float>().data(),  /* ptr to data */
+                                  Ty.dim_size(0),           /* num_rows */
+                                  Ty.dim_size(1)            /* num_cols */);
+
+    auto eigen_res = Tx_mat * Ty_mat;
+    cout << "Eigen result: \n" << eigen_res << endl ;
+
+    cout << "Checking results ...\n";
+    for( auto row = 0 ; row < Tx.dim_size(0) ; row ++ )
+    {
+      for( auto col = 0 ; col < Ty.dim_size(1) ; col ++ ){
+        if( tf_res(row, col) != eigen_res(row, col) )
+        { cout << "Results mismatch!\n"; return -1; }
+      }
+    }
+    cout << "Results match!\n";
 
     session->Close();
-    delete session;
     return 0;
 }
