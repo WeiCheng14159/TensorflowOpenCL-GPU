76df82b8ddac4b3210e2b266b025de8dd9729661
Wei Cheng <michael1996623@gmail.com>
2018-03-03 16:17:26 +0800

    maint-clean up bash file

--- tensorflow/cpu-mlp-trainer/android_build.sh
+++ tensorflow/cpu-mlp-trainer/android_build.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 
-TARGET=${PWD##*/} 
+TARGET=${PWD##*/}
 
-bazel build :$TARGET \
-   --verbose_failures \
-   --sandbox_debug \
-   --crosstool_top=//external:android/crosstool \
-   --host_crosstool_top=@bazel_tools//tools/cpp:toolchain \
-   --cpu=arm64-v8a \
-   --cxxopt="-std=c++11" \
-   --jobs=4 
+bazel build --config=android_arm64 :$TARGET \
+    --verbose_failures \
+    --cxxopt="-std=c++11" \
+    --cxxopt="-DSELECTIVE_REGISTRATION" \
+    --cxxopt="-DSUPPORT_SELECTIVE_REGISTRATION" \
+    --cxxopt="-DTEST_CL" \
+    --cxxopt="-DOPENCL_API" \
+    --jobs=8
 
 adb push ../../bazel-bin/tensorflow/"$TARGET"/"$TARGET" /data/local/tmp
 adb push models/mlp.pb /data/local/tmp
-
