92e1282c632f73d66c2f935ce736247be8853be6
Wei Cheng <michael1996623@gmail.com>
2018-03-03 15:51:03 +0800

    maint-code cleanup

--- tensorflow/opencl-matmul/android_build.sh
+++ tensorflow/opencl-matmul/android_build.sh
@@ -3,13 +3,13 @@
 TARGET=${PWD##*/}
 
 bazel build --config=android_arm64 :$TARGET \
-   --verbose_failures \
-   --cxxopt="-std=c++11" \
-   --cxxopt="-DSELECTIVE_REGISTRATION" \
-   --cxxopt="-DSUPPORT_SELECTIVE_REGISTRATION" \
-   --cxxopt="-DTEST_CL" \
-   --cxxopt="-DOPENCL_API" \
-   --jobs=8
+    --verbose_failures \
+    --cxxopt="-std=c++11" \
+    --cxxopt="-DSELECTIVE_REGISTRATION" \
+    --cxxopt="-DSUPPORT_SELECTIVE_REGISTRATION" \
+    --cxxopt="-DTEST_CL" \
+    --cxxopt="-DOPENCL_API" \
+    --jobs=8
 
 REMOTE_DIR="/data/local/tmp"
 adb push ../../bazel-bin/tensorflow/$TARGET/$TARGET $REMOTE_DIR
