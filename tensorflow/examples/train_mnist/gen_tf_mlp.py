c4c4d4496c375ae71fbfc7a2ebea222d25de50e1
Wei Cheng <michael1996623@gmail.com>
2018-03-26 11:44:27 +0800

    feat-Update MNIST MLP tf model
    * Update MLP model & code cleanup

--- tensorflow/examples/train_mnist/gen_tf_mlp.py
+++ tensorflow/examples/train_mnist/gen_tf_mlp.py
@@ -24,79 +24,120 @@ from __future__ import print_function
 
 import argparse
 import sys
-
+import tensorflow as tf
 from tensorflow.examples.tutorials.mnist import input_data
 
-import tensorflow as tf
-
-FLAGS = None
-
-img_size = 28
-
 def main(_):
-  # Import data
-  mnist = input_data.read_data_sets(FLAGS.data_dir)
 
-  # Create the model
-  x = tf.placeholder(tf.float32, [None, img_size * img_size ], name="input")
-  y = tf.placeholder(tf.int64,   [None                      ], name="output")
+    # Import data
+    mnist = input_data.read_data_sets(FLAGS.mnistDataDir, one_hot=True)
 
-  W = tf.Variable(tf.truncated_normal([ img_size * img_size, 10], stddev=0.1), name="W")
-  b = tf.Variable(tf.zeros([10]), name="b")
+    # Training parameters
+    maxEpochs = FLAGS.maxEpochs
+    batchSize = FLAGS.batchSize
+    testStep = FLAGS.testStep
 
-  y_out = tf.matmul(x, W) + b
+    # Network parameters
+    n_hidden_1 = 50 # 1st layer number of neurons
+    n_hidden_2 = 50 # 2nd layer number of neurons
+    n_input = 784 # MNIST data input (img shape: 28*28)
+    n_classes = 10 # MNIST total classes (0-9 digits)
 
-  # Define loss and optimizer
-  cross_entropy = tf.losses.sparse_softmax_cross_entropy(labels=y, logits=y_out)
-  with tf.name_scope('adam_optimizer'):
-      train_step = tf.train.AdamOptimizer().minimize(cross_entropy, name="train")
+    # tf Graph input
+    X = tf.placeholder(tf.float32, [None, n_input], name="input")
+    Y = tf.placeholder(tf.float32, [None, n_classes], name="output")
 
-  correct_prediction = tf.equal(tf.argmax(y_out, 1), y)
-  accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32), name="test")
+    # Store layers weight & bias
+    weights = {
+        'h1': tf.Variable(tf.random_normal([n_input, n_hidden_1])),
+        'h2': tf.Variable(tf.random_normal([n_hidden_1, n_hidden_2])),
+        'out': tf.Variable(tf.random_normal([n_hidden_2, n_classes]))
+    }
+    biases = {
+        'b1': tf.Variable(tf.random_normal([n_hidden_1])),
+        'b2': tf.Variable(tf.random_normal([n_hidden_2])),
+        'out': tf.Variable(tf.random_normal([n_classes]))
+    }
 
-  # Create a summary to monitor cross_entropy tensor
-  tf.summary.scalar("cross_entropy", cross_entropy)
-  # Create a summary to monitor accuracy tensor
-  tf.summary.scalar("accuracy", accuracy)
-  # Create a summary histogram for weight matrix
-  tf.summary.histogram("W", W)
-  # Create a summary histogram for biased vector
-  tf.summary.histogram("b", b)
+    # Create model
+    def multilayer_perceptron(x):
+        # Hidden fully connected layer with `n_hidden_1` neurons
+        layer_1 = tf.add(tf.matmul(x, weights['h1']), biases['b1'])
+        # Hidden fully connected layer with `n_hidden_2` neurons
+        layer_2 = tf.add(tf.matmul(layer_1, weights['h2']), biases['b2'])
+        # Output fully connected layer with a neuron for each class
+        out_layer = tf.matmul(layer_2, weights['out']) + biases['out']
+        return out_layer
 
-  # Merge all summaries into a single op
-  merged_summary_op = tf.summary.merge_all()
+    # Construct model
+    logits = multilayer_perceptron(X)
 
-  init_op = tf.initialize_variables(tf.all_variables(), name='init')
+    # Define loss
+    loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(
+        logits=logits, labels=Y))
 
-  sess = tf.Session()
-  sess.run(init_op)
+    # Define optimizer
+    with tf.name_scope('adam_optimizer'):
+        train_op = tf.train.AdamOptimizer().minimize(loss, name="train")
 
-  summary_writer = tf.summary.FileWriter( FLAGS.log_dir, graph=sess.graph)
+    # Define accuracy
+    prediction = tf.equal(tf.argmax(logits, 1), tf.argmax(Y, 1))
+    accuracy = tf.reduce_mean(tf.cast(prediction, tf.float32), name="test")
 
-  # Train
-  for step in range( FLAGS.max_steps ):
-    batch_xs, batch_ys = mnist.train.next_batch( FLAGS.batch_size )
-    if step % 100 == 0:
-        acc, summary = sess.run([ accuracy, merged_summary_op ], \
-            feed_dict={ x: mnist.test.images, y: mnist.test.labels })
-        print('step %d, training accuracy %f' % (step, acc))
-        summary_writer.add_summary(summary, step)
-    train_step.run( feed_dict={ x: batch_xs, y: batch_ys }, session=sess)
-  print('test accuracy %f' %  accuracy.eval(feed_dict={
-      x: mnist.test.images, y: mnist.test.labels }, session=sess))
-  tf.train.write_graph(sess.graph_def,
-                        './',
-                        'mnist_mlp.pb', as_text=False)
+    # Create a summary to monitor cross_entropy tensor
+    tf.summary.scalar("loss", loss)
+    # Create a summary to monitor accuracy tensor
+    tf.summary.scalar("accuracy", accuracy)
+    # Merge all summaries into a single op
+    merged_summary_op = tf.summary.merge_all()
+
+    # Initializing the variables
+    init = tf.initialize_variables(tf.all_variables(), name='init')
+
+    with tf.Session() as sess:
+        # Session Init
+        sess.run(init)
+
+        # Logger Init
+        summary_writer = tf.summary.FileWriter( FLAGS.logDir, graph=sess.graph)
+
+        # Training
+        for step in range(maxEpochs):
+            # Get MNIST training data
+            batchImage, batchLabel = mnist.train.next_batch(batchSize)
+
+            # Test training model for every testStep
+            if step % testStep == 0:
+                # Run accuracy op & summary op to get accuracy & training progress
+                acc, summary = sess.run([accuracy, merged_summary_op],
+                    feed_dict={X: mnist.test.images, Y: mnist.test.labels})
+
+                # Write accuracy to log file
+                summary_writer.add_summary(summary, step)
+
+                # Print accuracy
+                print('step %d, training accuracy %f' % (step, acc))
+
+            # Run training op
+            train_op.run( feed_dict={ X: batchImage, Y: batchLabel }, session=sess)
+
+        # Write TF model
+        tf.train.write_graph(sess.graph_def,
+                            './',
+                            'mnist_mlp.pb', as_text=False)
 
 if __name__ == '__main__':
-  parser = argparse.ArgumentParser()
-  parser.add_argument('--data_dir', type=str, default='/tmp/tensorflow/mnist/input_data',
-                      help='Directory for storing input data')
-  parser.add_argument('--log_dir', type=str, default='/tmp/tensorflow_logs/mlpnet',
-                      help='Directory for training data')
-  parser.add_argument('--batch_size', type=int, default=50,
-                      help='Batch size')
-  parser.add_argument('--max_steps', type=int, default=2000,
-                      help='Maximum training steps')
-  FLAGS, unparsed = parser.parse_known_args()
-  tf.app.run(main=main, argv=[sys.argv[0]] + unparsed)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--mnistDataDir', type=str, default='/tmp/tensorflow/mnist/input_data',
+                        help='MNIST data directory')
+    parser.add_argument('--logDir', type=str, default='/tmp/tensorflow_logs/mlpnet',
+                        help='Training progress data directory')
+    parser.add_argument('--batchSize', type=int, default=50,
+                        help='Training batch size')
+    parser.add_argument('--maxEpochs', type=int, default=10000,
+                        help='Maximum training steps')
+    parser.add_argument('--testStep', type=int, default=100,
+                        help='Test model accuracy for every testStep iterations')
+    FLAGS, unparsed = parser.parse_known_args()
+    # Program entry 
+    tf.app.run(main=main, argv=[sys.argv[0]] + unparsed)
